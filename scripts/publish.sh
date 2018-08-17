341a18444b5a47c6d4ebb4a2cd5ab31cb65b0de5
Eduardo Garcia <eduardo.garcia@esailors.de>
2018-08-17 21:32:13 +0000

    (blog) Add publish script

---
+++ scripts/publish.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Should be called from the project's root directory
+./scripts/build.sh
+git checkout -B gh-pages
+git add . && git commit -m "Release `git log --pretty=format:'%h' -n 1` (`date '+%Y-%m-%d %H:%M'`) [ci skip]"
+git push -f origin gh-pages
+git co master
