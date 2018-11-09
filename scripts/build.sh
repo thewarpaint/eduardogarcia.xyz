af39bb8a2c744dead7cd1c37bcfec7bb0f6a185f
Eduardo Garcia <eduardo.garcia@esailors.de>
2018-11-09 06:06:43 +0000

    (geometry) Add custom fingerprinting (cache busting)

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,3 +10,16 @@ for filename in _posts/*.html; do
   sed -i -e "s/%LATEST_SHA_SHORT%/$LATEST_SHA_SHORT/g" $filename
   sed -i -e "s/%DATE_READABLE%/$DATE_READABLE/g" $filename
 done
+
+# Custom Geometry Club fingerprinting
+GEOMETRY_CLUB_HTML="_posts/2018-11-01-geometry-club.html"
+GEOMETRY_CLUB_JS="assets/js/geometry-club.js"
+GEOMETRY_CLUB_JS_SHA="$(git log --pretty=format:'%H' -n 1 -- $GEOMETRY_CLUB_JS)"
+GEOMETRY_CLUB_CSS="assets/css/geometry-club.css"
+GEOMETRY_CLUB_CSS_SHA="$(git log --pretty=format:'%H' -n 1 -- $GEOMETRY_CLUB_CSS)"
+
+mv $GEOMETRY_CLUB_JS "$GEOMETRY_CLUB_JS.$GEOMETRY_CLUB_JS_SHA.js"
+sed -i -e "s/geometry-club.js/geometry-club.js.$GEOMETRY_CLUB_JS_SHA.js/g" $GEOMETRY_CLUB_HTML
+
+mv $GEOMETRY_CLUB_CSS "$GEOMETRY_CLUB_CSS.$GEOMETRY_CLUB_CSS_SHA.css"
+sed -i -e "s/geometry-club.css/geometry-club.css.$GEOMETRY_CLUB_CSS_SHA.css/g" $GEOMETRY_CLUB_HTML
