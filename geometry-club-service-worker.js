42f7a95b27d87089155721acba86dae628c82ba0
Eduardo Garcia <eduardo.garcia@esailors.de>
2018-11-24 14:36:20 +0000

    (geometry) Add cache assets, clear old caches logic

--- geometry-club-service-worker.js
+++ geometry-club-service-worker.js
@@ -1,7 +1,52 @@
-[
-  'assets/js/main.js',
-  'assets/js/geometry-club.js',
-  'assets/css/main.css',
-  'assets/css/geometry-club.css',
-  'assets/images/geometry-club-preview.png',
-]
+var CACHE_VERSION = 'cache-v%CACHE_VERSION%';
+
+self.addEventListener('install', function (event) {
+  addAssetsToCache(event);
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys()
+      .then(removeOldCaches)
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+function addAssetsToCache(event) {
+  event.waitUntil(
+    caches.open(CACHE_VERSION)
+      .then(function (cache) {
+        console.log('Cache ' + CACHE_VERSION + ' added');
+
+        return cache.addAll([
+          '/geometry-club',
+          '/assets/js/main.js',
+          '/assets/js/geometry-club.js',
+          '/assets/css/main.css',
+          '/assets/css/geometry-club.css',
+          '/assets/images/geometry-club-preview.jpg',
+          '/assets/images/geometry-club-preview--128-128.jpg',
+        ]);
+      })
+  );
+}
+
+function removeOldCaches(cacheKeys) {
+  return Promise.all(
+    cacheKeys
+      .filter(function (cacheKey) {
+        return cacheKey !== CACHE_VERSION;
+      })
+      .map(function (cacheKey) {
+        return caches.delete(cacheKey);
+      })
+  );
+}
