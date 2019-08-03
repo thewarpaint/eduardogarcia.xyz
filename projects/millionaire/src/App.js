0f45578b9ad8a3c26b607979bc8bc406659b5d83
Eduardo Garcia <eduardogarcia.xyz+gh@gmail.com>
2019-08-02 21:14:53 -0500

    (blog millionaire) Add millionaire React project

---
+++ projects/millionaire/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <h1 class="title">
+      How much money do you need to be a millionaire in 🇲🇽 Mexico?
+    </h1>
+  );
+}
+
+export default App;
