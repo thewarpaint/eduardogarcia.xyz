b096965385a1dc1a5b6625ca7b2239d4737964ed
Eduardo Garcia <eduardogarcia.xyz+gh@gmail.com>
2021-12-20 00:03:57 -0600

    (fotuit) Add server

---
+++ projects/fotuit/server.js
@@ -0,0 +1,67 @@
+import fastify from 'fastify';
+import fastifyStatic from 'fastify-static';
+import handlebars from 'handlebars';
+import path from 'path';
+import pointOfView from 'point-of-view';
+
+import {
+  buildFotuitUrl,
+  getPayloadFromTweetId,
+} from './fotuit.js';
+
+import {
+  getPageScreenshot,
+} from './screenshot.js';
+
+const fastifyServer = fastify({
+  logger: false,
+});
+
+fastifyServer.post('/v1/fotuit', async function(request, reply) {
+  const {
+    tweetId,
+    backgroundImageUrl,
+  } = request.body;
+
+  const payload = {
+    ...await getPayloadFromTweetId(tweetId),
+    backgroundImageUrl,
+  };
+
+  const fotuitUrl = buildFotuitUrl(payload);
+
+  const screenshot = await getPageScreenshot(fotuitUrl);
+
+  reply
+    .type('png')
+    .send(screenshot);
+});
+
+// TODO: bring back after static files are added
+// Setup our static files
+// fastifyServer.register(fastifyStatic, {
+//   root: path.join(__dirname, 'public'),
+//   prefix: '/'
+// });
+
+// point-of-view is a templating manager for fastify
+fastifyServer.register(pointOfView, {
+  engine: {
+    handlebars,
+  }
+});
+
+// Our main GET home page route, pulls from src/pages/index.hbs
+fastifyServer.get('/', function(request, reply) {
+  reply.view('/src/pages/index.hbs', {});
+});
+
+// Run the server and report out to the logs
+fastifyServer.listen(process.env.PORT, function(err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  console.log(`Your app is listening on ${address}`);
+  fastifyServer.log.info(`server listening on ${address}`);
+});
